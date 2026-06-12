c707080d031a88ca0774ddcab79d36ac0b3f2ea7
Tara Kadambi <your-email@example.com>
2026-06-12 16:18:16 +0200

    add root route

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func contactPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	http.HandleFunc("/", homePage)
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
